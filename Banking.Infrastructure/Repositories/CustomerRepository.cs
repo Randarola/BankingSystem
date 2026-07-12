acf8712f273d8796decb05d6241b3b77c0aa2493
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 20:35:28 +0200

    Update CustomerRepository.cs

--- Banking.Infrastructure/Repositories/CustomerRepository.cs
+++ Banking.Infrastructure/Repositories/CustomerRepository.cs
@@ -2,5 +2,34 @@ namespace Banking.Infrastructure.Repositories;
 
 public class CustomerRepository
 {
+    public Customer? GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Customer? GetByPesel(string pesel)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Customer> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Customer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Customer customer)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Delete(int id)
+    {
+        throw new NotImplementedException();
+    }
 
 }
