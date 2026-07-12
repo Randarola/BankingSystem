338b768953b91f9a8f5ed8b63f242ab936bf82ed
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 20:33:53 +0200

    Update AccountRepository.cs

--- Banking.Infrastructure/Repositories/AccountRepository.cs
+++ Banking.Infrastructure/Repositories/AccountRepository.cs
@@ -2,5 +2,29 @@ namespace Banking.Infrastructure.Repositories;
 
 public class AccountRepository
 {
-        
+    public Account? GetByAccountNumber(string accountNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Account? GetByCustomerId(int customerId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Account> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Account account)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(Account account)
+    {
+        throw new NotImplementedException();
+    }
+
 }
