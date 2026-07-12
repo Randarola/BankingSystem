0871b737e382e46e1ae0fb24ff8acdaa371c1393
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 23:53:01 +0200

    Update Program.cs

--- Banking.Server/Program.cs
+++ Banking.Server/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Banking.Server
+﻿using Banking.Infrastructure.Repositories;
+
+namespace Banking.Server
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            var repository = new CustomerRepository();
+
+            var customers = repository.GetAll();
+
+            foreach (var customer in customers)
+            {
+                Console.WriteLine($"{customer.Id} {customer.FirstName} {customer.LastName}");
+            }
         }
     }
 }
