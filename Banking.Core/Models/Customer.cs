8d41eb8058385ad2e9eb287bf8355f6d56c2943a
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 13:24:40 +0200

    Update Customer.cs

--- Banking.Core/Models/Customer.cs
+++ Banking.Core/Models/Customer.cs
@@ -1,8 +1,20 @@
 ﻿using System;
 
-public class Class1
+public class Customer
 {
-	public Class1()
+    public int Id { get; private set; }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Pesel { get; private set; }
+    public string PasswordHash { get; private set; }
+
+    public Customer(int id, string firstName, string lastName, string pesel, string passwordHash)
 	{
+        Id = id;
+		FirstName = firstName;
+        LastName = lastName;
+        Pesel = pesel;
+        PasswordHash = passwordHash;
 	}
+	
 }
