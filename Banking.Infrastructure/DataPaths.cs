c2de940106fa748239524c076d8a20dd0b8512bd
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 23:52:47 +0200

    Create DataPaths.cs

---
+++ Banking.Infrastructure/DataPaths.cs
@@ -0,0 +1,9 @@
+﻿namespace Banking.Infrastructure
+{
+    public static class DataPaths
+    {
+        public const string Customers = @"..\..\..\..\Banking.Infrastructure\Data\customers.json";
+        public const string Accounts = @"..\..\..\..\Banking.Infrastructure\Data\accounts.json";
+        public const string Transactions = @"..\..\..\..\Banking.Infrastructure\Data\transactions.json";
+    }
+}
