83f77a345bb0078f265ef990b591c3e65e5dd8ba
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 13:44:53 +0200

    Update Account.cs

--- Banking.Core/Models/Account.cs
+++ Banking.Core/Models/Account.cs
@@ -2,14 +2,12 @@ namespace Banking.Core.Models;
 
 public class Account
 {
-	public int Id { get; private set; }
 	public string AccountNumber { get; private set; }
 	public int CustomerId { get; private set; }
 	public decimal Balance { get; private set; }
 
 	public Account(int id, string accountNumber, int customerId, decimal balance)
 	{
-		Id = id;
 		AccountNumber = accountNumber;
 		CustomerId = customerId;
 		Balance = balance;
