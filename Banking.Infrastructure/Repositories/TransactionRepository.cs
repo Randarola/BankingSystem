78b4632fea20f7d828063073c7e62de6219a7e8f
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 20:37:15 +0200

    Update TransactionRepository.cs

--- Banking.Infrastructure/Repositories/TransactionRepository.cs
+++ Banking.Infrastructure/Repositories/TransactionRepository.cs
@@ -2,5 +2,19 @@ namespace Banking.Infrastructure.Repositories;
 
 public class TransactionRepository
 {
+    public List<Transaction> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public List<Transaction> GetByAccountNumber(string accountNumber)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Add(Transaction transaction)
+    {
+        throw new NotImplementedException();
+    }
 
 }
