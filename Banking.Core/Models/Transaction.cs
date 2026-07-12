34c166d45cfb4d34c78152af0c15f686da9039d8
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-12 18:40:20 +0200

    Update Transaction.cs

--- Banking.Core/Models/Transaction.cs
+++ Banking.Core/Models/Transaction.cs
@@ -13,17 +13,18 @@ public class Transaction
 
     public Transaction(
         int id,
-        TransactionType type,
-        string? sourceAccountNumber,
-        string? destinationAccountNumber,
-        decimal amount,
-        DateTime timestamp)
+        TransactionType type, string? sourceAccountNumber, string? destinationAccountNumber, decimal amount)
     {
+
+        if (amount <= 0)
+        {
+            throw new ArgumentOutOfRangeException(nameof(amount), "Amount must be greater than zero.");
+        }
         Id = id;
         Type = type;
         SourceAccountNumber = sourceAccountNumber;
         DestinationAccountNumber = destinationAccountNumber;
         Amount = amount;
-        Timestamp = timestamp;
+        Timestamp = DateTime.UtcNow;
     }
 }
