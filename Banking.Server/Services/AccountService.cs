8ec2471dde6b534d931527d70b6186ec90826cb2
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-13 12:33:42 +0200

    Create AccountService.cs

---
+++ Banking.Server/Services/AccountService.cs
@@ -0,0 +1,18 @@
+﻿using Banking.Core.Models;
+using Banking.Core.Enums;
+using Banking.Infrastructure.Repositories;
+
+namespace Banking.Server.Services
+{
+    public class AccountService
+    {
+        private readonly AccountRepository _accountRepository;
+        private readonly TransactionRepository _transactionRepository;
+
+        public AccountService(AccountRepository accountRepository, TransactionRepository transactionRepository)
+        {
+            _accountRepository = accountRepository;
+            _transactionRepository = transactionRepository;
+        }   
+    }
+}
