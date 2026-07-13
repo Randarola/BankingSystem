69c60af9423378605bdee993c086d69fb211bca4
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-13 11:33:26 +0200

    Create AuthService.cs

---
+++ Banking.Server/Services/AuthService.cs
@@ -0,0 +1,29 @@
+﻿using Banking.Core.Models;
+using Banking.Infrastructure.Repositories;
+
+namespace Banking.Server.Services;
+public class AuthService
+{
+    private readonly CustomerRepository _customerRepository;
+    public AuthService(CustomerRepository customerRepository)
+    {
+        _customerRepository = customerRepository;
+    }
+
+    public Customer? Login(string pesel, string password)
+    {
+        var customer = _customerRepository.GetByPesel(pesel);
+
+        if (customer == null)
+        {
+            return null;
+        }
+
+        if (customer.PasswordHash != password)
+        {
+            return null;
+        }
+        return customer;
+    }
+}
+
