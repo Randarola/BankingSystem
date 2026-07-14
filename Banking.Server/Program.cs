87544f64138ff817eff28e0aa004736ed4ca32fd
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-14 09:13:35 +0200

    Update Program.cs

--- Banking.Server/Program.cs
+++ Banking.Server/Program.cs
@@ -1,20 +1,13 @@
-﻿using Banking.Infrastructure.Repositories;
+﻿using Banking.Server.Networking;
 
-namespace Banking.Server
+namespace Banking.Server;
+
+internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
+        var server = new TcpServer(5000);
 
-            var repository = new CustomerRepository();
-
-            var customers = repository.GetAll();
-
-            foreach (var customer in customers)
-            {
-                Console.WriteLine($"{customer.Id} {customer.FirstName} {customer.LastName}");
-            }
-        }
+        server.Start();
     }
 }
