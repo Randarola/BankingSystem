406b82246ee9cb4455d0646c31ca3fd79c5393ff
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-14 09:13:14 +0200

    Create TcpServer.cs

---
+++ Banking.Server/Networking/TcpServer.cs
@@ -0,0 +1,25 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace Banking.Server.Networking
+{
+    public class TcpServer
+    {
+        private readonly TcpListener _listener;
+        public TcpServer(int port)
+        {
+            _listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        public void Start()
+        {
+            _listener.Start();
+            Console.WriteLine("Server started. Listening for connections...");
+            while (true)
+            {
+                var client = _listener.AcceptTcpClient();
+                Console.WriteLine("Client connected.");
+            }
+        }
+    }
+}
