f505896f3ccb3a9f41a5e9b8a59fa1cf32b14539
Randarola <140419343+Randarola@users.noreply.github.com>
2026-07-14 09:23:28 +0200

    Update Program.cs

--- Banking.Client/Program.cs
+++ Banking.Client/Program.cs
@@ -1,9 +1,20 @@
-﻿namespace Banking.Client
+﻿using System.Net.Sockets;
+namespace Banking.Client
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            try
+            {
+                TcpClient client = new TcpClient();
+                client.Connect("", 5000);
+                Console.WriteLine("Connected to the server.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Connection failed: {e.Message}");
+            }
 
         }
     }
